68e5cb2a8d712e9169627ff95c8b20de5e97073e
Kian Edalat <akedalat@gmail.com>
2019-01-26 19:02:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, value_hash|
     value_hash.each do |color, name_array|
       name_array.each do |name|
-
-
-    binding.pry
-  end
+        pigeon_name = name
+            binding.pry
+      end
+    end
   end
 end
